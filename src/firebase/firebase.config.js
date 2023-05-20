f04a212b9e7514fc6531c6477753fbb9d4014d0a
Abu Motaleb <abumotaleb99@gmail.com>
2023-05-21 04:33:29 +0600

    added firebase config file

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAK7etwhJ35vcCRvPF2kG4VBhCyNWM2CPg",
+  authDomain: "toy-zone-47adc.firebaseapp.com",
+  projectId: "toy-zone-47adc",
+  storageBucket: "toy-zone-47adc.appspot.com",
+  messagingSenderId: "712415943260",
+  appId: "1:712415943260:web:49f7fb0529f5231c79a499",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
