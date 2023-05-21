5c863c7c7870eef8e7fcee6676112fb53cbc5666
Abu Motaleb <abumotaleb99@gmail.com>
2023-05-21 18:54:29 +0600

    added shop by category section and loaded dynacim data

---
+++ src/pages/Home/ShopByCategory.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import Toy from "./Toy";
+
+const ShopByCategory = () => {
+  const [toys, setToys] = useState([]);
+  const [activeTab, setActiveTab] = useState("All");
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/allToysByCategory/${activeTab}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setToys(result);
+      });
+  }, [activeTab]);
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
+  return (
+    <div className="max-w-7xl mx-auto px-5 md:px-0 pb-10 md:pb-24">
+      <h3 className="text-2xl md:text-3xl font-bold text-center pb-8 md:pb-12">
+        Shop By Category
+      </h3>
+      <div className="flex justify-center pb-5">
+        <ul className="flex gap-1">
+          <li>
+            <button
+              onClick={() => handleTabClick("All")}
+              className={`text-white bg-[#29DE92] px-5 py-2 rounded-md ${
+                activeTab == "All" ? "bg-[#32A575]" : ""
+              }`}
+            >
+              All
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => handleTabClick("Car")}
+              className={`text-white bg-[#29DE92] px-5 py-2 rounded-md  ${
+                activeTab == "Car" ? "bg-[#32A575]" : ""
+              }`}
+            >
+              Car
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => handleTabClick("Truck")}
+              className={`text-white bg-[#29DE92] px-5 py-2 rounded-md  ${
+                activeTab == "Truck" ? "bg-[#32A575]" : ""
+              }`}
+            >
+              Truck
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => handleTabClick("Racing Car")}
+              className={`text-white bg-[#29DE92] px-5 py-2 rounded-md  ${
+                activeTab == "Racing Car" ? "bg-[#32A575]" : ""
+              }`}
+            >
+              Police Car
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        {toys.map((toy) => (
+          <Toy key={toy._id} toy={toy}></Toy>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ShopByCategory;
---
+++ src/pages/Home/Toy.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "react-rating";
+import { FaRegStar, FaStar } from "react-icons/fa";
+
+const Toy = ({ toy }) => {
+  const { _id, toyName, sellerName, photoUrl, price, rating } = toy;
+  return (
+    <div className="bg-[#F5F5F5] rounded-lg px-5 pt-5 pb-3">
+      <div>
+        <img src={photoUrl} className="rounded-md" alt="" />
+      </div>
+      <div className="my-3">
+        <h2 className="text-base font-medium">{toyName}</h2>
+        <Rating
+          placeholderRating={rating}
+          emptySymbol={<FaRegStar></FaRegStar>}
+          readonly
+          placeholderSymbol={<FaStar className="text-amber-400"></FaStar>}
+          fullSymbol={<FaStar></FaStar>}
+        ></Rating>
+        <p className="text-lg font-bold pb-4">
+          <span>{price}</span> TK
+        </p>
+        <Link
+          to=""
+          className="w-full block text-center text-white bg-[#29DE92] hover:bg-[#32A575] rounded-md px-7 py-2"
+        >
+          View Toy
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Toy;
